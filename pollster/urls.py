cd27ec05dee56ca23f3f4b2ad6cae1c8749beeff
Abdirayimov <orifjon12032000@gmail.com>
2021-10-28 10:15:09 +0500

    added static files

--- pollster/urls.py
+++ pollster/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,4 +7,4 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
